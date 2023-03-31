7507a772f542dcfcbc6728d5e574988dea867a56
Mika Senghaas <accounts@jonassenghaas.de>
2023-03-31 15:25:18 +0200

    feat(run): add poetry to path and change to project dir

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# add poetry to path
+export PATH="${HOME}/.local/bin:${PATH}"
+
+# change to project dir
+PROJECT_DIR=$(dirname "$(readlink -f "$0")")
+cd ${PROJECT_DIR}
+
+# install dependencies
 poetry install
+
+# run experiment
 poetry run python main.py
